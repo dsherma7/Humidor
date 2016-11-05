09b795321f753afcb1e5c4f4b2958847bead05fb
Doug Sherman <dsherman@ucdavis.edu>
2016-11-04 20:18:38 -0700

    Update Cigar Parse Function

---
+++ humidor/parse_cigar.py
@@ -0,0 +1,8 @@
+def splitCigar(self, cigar):
+    self.cigar = parseCigar(cigar)
+
+    def parseCigar(self,cigar):
+    	return cigar
+
+cig = cigarString('M4I10M22S15')
+print(cig.cigar)
--- humidor/read_lines.py
+++ humidor/read_lines.py
@@ -6,43 +6,6 @@ def readlines(ref):
     p = str(subprocess.check_output(cmd + " " + ref,shell=True))
     return [p, p.count('\\n')]
 
-class cigarString:
-    """ Class to parse and handle sam formatted cigar strings """
-    pattern = re.compile('([MIDNSHPX=])')
-
-    def __init__(self, cigar):
-        values = self.pattern.split(cigar)[:-1]
-        self.paired = (values[n:n+2] for n in xrange(0, len(values), 2))  # pair values by twos
-
-    def getAlignmentLength(self):
-        g = 0
-        for pair in self.paired:
-            l = int(pair[0])
-            t = pair[1]
-            if t == 'M':
-                g += l
-            elif t == 'I':
-                pass
-            elif t == 'D':
-                g += l
-            elif t == 'N':
-                pass
-            elif t == 'S':
-                pass
-            elif t == 'H':
-                pass
-            elif t == 'P':
-                pass
-            elif t == 'X':
-                pass
-            elif t == '=':
-                pass
-            else:
-                sys.stderr.write("encountered unhandled CIGAR character %s\n" % t)
-                pass
-        return g
-
-
-
 [file, num_lines] = readlines(sys.argv[1])
 print(num_lines)
+
